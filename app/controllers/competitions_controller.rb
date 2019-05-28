f8445e852a147a41b3cdb2c1fc631afb45d4f286
miaoshan <minnieshan@gmail.com>
2019-05-28 18:03:09 +0100

    controllers

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -1,5 +1,62 @@
 class CompetitionsController < ApplicationController
 
+  before_action :find_competitions, only:[:show,:edit,:update,:destroy]
+
+
   def index
+    @competitions = Competition.all
+
   end
+
+
+  def new
+    @competition = Competition.new
+
+  end
+
+  def show
+    @competition = Competition.find_competition
+  end
+
+  def create
+    @competition = Competition.new(competition_params)
+    if @competition.valid?
+      @competition.save
+      redirect_to competition_path(@competition)
+    else
+      flash[:errors]=@competition.errors.full_messages
+      redirect_to new_competition_path
+    end
+  end
+
+  def update
+    @competition.update(competition_params)
+    if @competition.valid?
+      redirect_to @competition
+    else
+      flash[:errors] = @competition.errors.full_messages
+      redirect_to edit_competition_path
+    end
+  end
+
+
+  def destroy
+  @competition = Competition.find_competition
+  @competition.destroy
+  redirect_to competitions_path
+end 
+
+
+  
+  private
+
+  def find_competition
+    Competition.find(params[:id])
+  end
+
+  def competition_params
+    params.require(:competition).permit(:name,:knockout)
+  end
+end
+
 end
