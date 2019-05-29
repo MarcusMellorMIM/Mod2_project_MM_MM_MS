5bf2784d2bc594375f639f34660abc333e815b3f
MarcoMiranda05 <marmirand@hotmail.com>
2019-05-29 16:04:40 +0100

    team MVC working

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,31 +1,35 @@
 class CampaignsController < ApplicationController
     before_action :find_campaign, only:[:show,:edit,:update,:destroy]
 
-
     def index
       @campaigns = Campaign.all
-  
     end
   
   
     def new
       @campaign = Campaign.new
-  
+      @team = Team.find(params[:team_id])
     end
   
     def show
-      @campaign = Campaign.find_campaign
     end
   
     def create
-      @campaign = Campaign.new(campaign_params)
-      if @campaign.valid?
-        @campaign.save
-        redirect_to campaign_path(@campaign)
-      else
-        flash[:errors]=@campaign.errors.full_messages
-        redirect_to new_campaign_path
+      params[:competition_ids].each do |competition_id|
+        Campaign.create({:team_id => params[:team_id], :competition_id => competition_id})
       end
+      redirect_to team_path(params[:team_id])
+      
+      # @campaign = Campaign.new(campaign_params)
+      # if @campaign.valid?
+      #   @campaign.save
+      #   redirect_to campaign_path(@campaign)
+      # else
+      #   flash[:errors]=@campaign.errors.full_messages
+      #   redirect_to new_campaign_path
+    end
+
+    def edit
     end
   
     def update
@@ -40,7 +44,6 @@ class CampaignsController < ApplicationController
   
   
     def destroy
-    @campaign = Campaign.find_campaign
     @campaign.destroy
     redirect_to campaigns_path
   end 
@@ -50,12 +53,11 @@ class CampaignsController < ApplicationController
     private
   
     def find_campaign
-      Campaign.find(params[:id])
+      @campaign = Campaign.find(params[:id])
     end
   
     def campaign_params
       params.require(:campaign).permit(:team_id,:competition_id)
     end
-  end
   
-end
+  end
